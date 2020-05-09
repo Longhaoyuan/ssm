56a1eacc46c1eb128f13d2260baf75dbdf187ca2
Longhaoyuan <65011133+Longhaoyuan@users.noreply.github.com>
2020-05-09 11:11:47 +0800

    Update HelloWord.java

--- HelloWord/src/com/company/HelloWord.java
+++ HelloWord/src/com/company/HelloWord.java
@@ -4,7 +4,8 @@ public class HelloWord {
     public static void main(String[] args) {
         // write your code here
         int username=90;
-        System.out.println("你们好啊"+90);
+        String usename='马云';
+        System.out.println("你们好啊"+90+"+username);
 
     }
 }
